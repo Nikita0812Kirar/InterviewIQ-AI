192fe39e69609e65c25cdc8b05fcd9fc1fedb1ae
Nikita Kirar <nikita.kirar04@gmail.com>
2026-05-17 13:36:57 +0530

    update for optimize rander backend

--- app/main.py
+++ app/main.py
@@ -371,4 +371,8 @@ if __name__ == "__main__":
 
     import uvicorn
 
-    uvicorn.run("app.main:app", host="127.0.0.1", port=int(os.getenv("PORT", "8000")))
+    uvicorn.run(
+    "app.main:app",
+    host="0.0.0.0",
+    port=int(os.getenv("PORT", "8000"))
+)
